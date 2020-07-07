9071bc32392ee0ba4f8f87b3d565c7486b33a6d9
mehdi-benhariz <benharizmehdi20@gmail.com>
2020-07-07 11:20:47 +0100

    Signed-off-by: mehdi-benhariz <benharizmehdi20@gmail.com>

---
+++ src/components/Bottom.jsx
@@ -0,0 +1,16 @@
+import {AppBar,Toolbar,Typography} from '@material-ui/core';
+import React from 'react'
+const Bar = () => {
+    return ( 
+  
+<AppBar position="static" >
+  <Toolbar>
+    <Typography variant="h6" >
+         By Mehdi Ben Hariz
+    </Typography>
+  </Toolbar>
+</AppBar>
+     );
+}
+ 
+export default Bar;
